eda05c04050bdb765d44ff28e4552ecf8176fdf2
wanghao2 <wanghao2@gomeholdings.com>
2017-12-27 14:35:06 +0800

    增加注解

--- src/main/java/com/wanghao/ioc/BeanFactory.java
+++ src/main/java/com/wanghao/ioc/BeanFactory.java
@@ -16,6 +16,7 @@ public class BeanFactory {
     /***
      * IOC最基本的俩个角色 容器(BeanFactory)和bean本身
      * 这里使用BeanDefinition来封装bean对象, 这样可以保存一些额外的元信息
+     * beanDefinitonMap 保存当前的元数据, 里面会很大很大的
      */
 
     private Map<String,BeanDefinition> beanDefinitonMap=new ConcurrentHashMap<String,BeanDefinition>();
